3f6430c20ae56c5f61c2453e72fb81310c60f176
Lorenzo Baracchi <baracchi.lorenzo@gmail.com>
2019-01-10 16:48:55 +0100

    replaced cocaine with terrapin

--- lib/av.rb
+++ lib/av.rb
@@ -5,30 +5,30 @@ require "av/cli"
 require "av/param_hash"
 require "av/commands/ffmpeg"
 require "av/commands/avconv"
-require "cocaine"
+require "terrapin"
 require "av/engine" if defined?(Rails)
 
 module Av
   extend self
   extend Logger
-  
+
   def options
     @options ||= {
       log: true,
       quiet: true,
     }
   end
-  
+
   def cli(options = {})
     @options = options unless options.empty?
     ::Av::Cli.new(options)
   end
-  
+
   def run line, codes = [0]
     ::Av.log("Running command: #{line}")
     begin
-      Cocaine::CommandLine.new(line, "", expected_outcodes: codes).run
-    rescue Cocaine::ExitStatusError => e
+      Terrapin::CommandLine.new(line, "", expected_outcodes: codes).run
+    rescue Terrapin::ExitStatusError => e
       raise Av::CommandError, "error while running command #{line}: #{e}"
     end
   end
